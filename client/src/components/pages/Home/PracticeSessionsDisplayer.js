56f4c3aed49a2dd6e0cea3550d06a9c2e46c52b0
Kenneth Tran <kennethtran77@gmail.com>
2022-02-22 00:59:44 -0500

    Fix typo

--- client/src/components/pages/Home/PracticeSessionsDisplayer.js
+++ client/src/components/pages/Home/PracticeSessionsDisplayer.js
@@ -37,7 +37,7 @@ const PracticeSessionsDisplayer = ({ userId }) => {
                 })}
                 { practiceSessionsToDisplay.length == 0 && (
                     <tr key={0}>
-                        <td colspan="3" className="padding">You have not completed any practice sessions.</td>
+                        <td colSpan="3" className="padding">You have not completed any practice sessions.</td>
                     </tr>
                 ) }
                 </tbody>
