07eebdaf79f94d286be6498aa7ffa7c08d81c671
Kenneth Tran <kennethtran77@gmail.com>
2022-06-29 22:33:35 -0400

    Move token generation logic into redux action

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes, Navigate } from 'react-router';
 
-import decode from 'jwt-decode';
-
-import { fetchConcepts } from './actions/concepts.js';
-import { fetchCollections } from './actions/collections.js';
-import { fetchPracticeSessions } from './actions/practice.js';
-import { fetchSettings, fetchUsernames } from './actions/user.js';
 import { generateToken } from './actions/auth.js';
-import { setAuthHeader } from './api/index.js';
 
 import './App.css';
 
@@ -27,38 +20,20 @@ import BrowseConcepts from './components/pages/BrowseConcepts';
 import ViewConcept from './components/pages/ViewConcept/ViewConcept';
 import Collections from './components/pages/Collections/Collections';
 import EditCollection from './components/pages/Collections/EditCollection';
-
 import PracticeCollection from './components/pages/Practice/PracticeCollection.js';
 
 const App = () => {
-    const token = useToken(); 
     const dispatch = useDispatch();
 
-    // Load data once user ID loads
+    // try and generate an access token with cookie refresh token upon App component mount
     useEffect(() => {
-        if (token) {
-            dispatch({ type: 'questions/clear' });
-            dispatch(fetchConcepts(token.id));
-            dispatch(fetchCollections());
-            dispatch(fetchPracticeSessions());
-            dispatch(fetchSettings());
-            dispatch(fetchUsernames());
-        }
-    }, [dispatch, token]);
+        dispatch(generateToken());
+    }, []);
 
-    // wrap the component in a fragment containing the navbar with the decoded
-    // token, and passing userId as props into the component
-    const wrap = (Component) => (
-        <>
-            <Navbar decodedToken={token} />
-            <div className="main">
-                { <Component userId={token.id} /> }
-            </div>
-        </>
-    );
+    const { token } = useSelector(state => state.authSlice);
 
     // limit routes when no jwt token is present
-    if (!token)
+    if (!token) {
         return (
             <Routes>
                 <Route path='*' element={<Navigate to="/" />}></Route>
@@ -67,56 +42,37 @@ const App = () => {
                 <Route path='/' element={<Login />}></Route>
             </Routes>
         );
+    }
 
-    return (
+    // wrap the component in a fragment containing the navbar with the decoded
+    // token, and passing userId as props into the component
+    const wrap = (Component) => (
         <>
-            <Routes>
-                <Route path='*' element={<Error />}></Route>
-                <Route exact path="/" element={ wrap(Home) }></Route>
-                <Route exact path="/login" element={<Navigate to="/" />}></Route>
-                <Route exact path="/signup" element={<Navigate to="/" />}></Route>
-                <Route path="/create" element={ wrap(EditConcept) }></Route>
-                <Route exact path="/concept/view/:conceptId" element={ wrap(ViewConcept) }></Route>
-                <Route exact path='/concept/edit/:conceptId' element={ wrap(EditConcept) }></Route>
-                <Route exact path='/practice/concept/:conceptId' element={ wrap(PracticeConcept) }></Route>
-                <Route exact path='/collection/edit/:collectionId' element={ wrap(EditCollection) }></Route>
-                <Route exact path='/practice/collection/:collectionId' element={ wrap(PracticeCollection) }></Route>
-                <Route exact path="/settings" element={ wrap(Settings) }></Route>
-                <Route exact path="/browse" element={ wrap(BrowseConcepts) }></Route>
-                <Route exact path="/collections" element={ wrap(Collections) }></Route>
-                <Route exact path="/practice/results/:sessionId" element={ wrap(PracticeResults) }></Route>
-            </Routes>
+            <Navbar userId={token.id} />
+            <main>
+                { <Component userId={token.id} /> }
+            </main>
         </>
     );
+
+    return (
+        <Routes>
+            <Route path='*' element={<Error />}></Route>
+            <Route exact path="/" element={ wrap(Home) }></Route>
+            <Route exact path="/login" element={<Navigate to="/" />}></Route>
+            <Route exact path="/signup" element={<Navigate to="/" />}></Route>
+            <Route path="/create" element={ wrap(EditConcept) }></Route>
+            <Route exact path="/concept/view/:conceptId" element={ wrap(ViewConcept) }></Route>
+            <Route exact path='/concept/edit/:conceptId' element={ wrap(EditConcept) }></Route>
+            <Route exact path='/practice/concept/:conceptId' element={ wrap(PracticeConcept) }></Route>
+            <Route exact path='/collection/edit/:collectionId' element={ wrap(EditCollection) }></Route>
+            <Route exact path='/practice/collection/:collectionId' element={ wrap(PracticeCollection) }></Route>
+            <Route exact path="/settings" element={ wrap(Settings) }></Route>
+            <Route exact path="/browse" element={ wrap(BrowseConcepts) }></Route>
+            <Route exact path="/collections" element={ wrap(Collections) }></Route>
+            <Route exact path="/practice/results/:sessionId" element={ wrap(PracticeResults) }></Route>
+        </Routes>
+    );
 };
-
-export const useToken = () => {
-    // decodedToken should be an object with key `id` representing the user's id
-    const [decodedToken, setDecodedToken] = useState(null);
-
-    const dispatch = useDispatch();
-
-    const refreshToken = () => {
-        dispatch(generateToken())
-        .then(token => {
-            if (token) {
-                const decoded = decode(token);
-                setDecodedToken(decoded);
-                setAuthHeader(token);
-
-                // generate a new access token immediately before expiration
-                setTimeout(() => {
-                    refreshToken();
-                }, ((decoded.exp * 1000) - Date.now()) - 500);
-            }
-        });
-    };
-
-    useEffect(() => {
-        refreshToken();
-    }, []);
-
-    return decodedToken;
-}
 
 export default App;
