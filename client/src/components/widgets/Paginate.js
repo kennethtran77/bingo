bb238928606d71777f2b57ba6d7f6afe3820e93a
Kenneth Tran <kennethtran77@gmail.com>
2022-07-03 15:49:23 -0400

    Add multiple page buttons

--- client/src/components/widgets/Paginate.js
+++ client/src/components/widgets/Paginate.js
@@ -1,31 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 import Button from './Button';
 
-const Paginate = ({ items, itemsPerPage, setItemsToDisplay }) => {
-    const [currentPage, setCurrentPage] = useState(0);
+const Paginate = ({ items, itemsPerPage = 5, setItemsToDisplay, maxPageButtons = 6 }) => {
+    const [currentPage, setCurrentPage] = useState(1);
     const [pageCount, setPageCount] = useState(0);
+
+    const [mobile, setMobile] = useState(false);
+
+    // trigger mobile pagination on width breakpoint
+    useLayoutEffect(() => {
+        const query = window.matchMedia("(max-width: 460px");
+        setMobile(query.matches);
+        query.addEventListener('change', e => setMobile(e.matches));
+    }, [])
 
     useEffect(() => {
         if (items) {
             const newPageCount = Math.ceil(items.length / itemsPerPage);
 
             setPageCount(newPageCount);
-            const pagesVisited = currentPage * itemsPerPage;
-            setItemsToDisplay(items.slice(pagesVisited, pagesVisited + itemsPerPage));
+            const pageFirstItemIndex = (currentPage - 1) * itemsPerPage;
+            setItemsToDisplay(items.slice(pageFirstItemIndex, pageFirstItemIndex + itemsPerPage));
 
             // update current page if items size changed
-            if (currentPage > 0 && currentPage > newPageCount - 1) {
-                setCurrentPage(newPageCount - 1);
+            if (currentPage > newPageCount) {
+                setCurrentPage(newPageCount);
+            } else if (currentPage <= 0) {
+                setCurrentPage(1);
             }
         }
     }, [currentPage, itemsPerPage, items, setItemsToDisplay]);
 
     const nextPage = () => {
-        if (currentPage < pageCount - 1) {
+        if (currentPage < pageCount) {
             setCurrentPage(currentPage + 1);
         }
     };
@@ -36,18 +47,64 @@ const Paginate = ({ items, itemsPerPage, setItemsToDisplay }) => {
         }
     };
 
+    const getPageButtons = () => {
+        let buttons = [];
+
+        let width = '35px';
+        let height = '35px';
+
+        const getButton = (page) => <Button key={page} onClick={() => setCurrentPage(page)} background={currentPage === page} text={page} width={width} height={height} align="center" />;
+
+        if (mobile) {
+            return getButton(currentPage);
+        }
+
+        if (currentPage <= maxPageButtons) {  // don't render button to 1st page
+            for (let i = 1; i <= Math.min(maxPageButtons, pageCount); i++) {
+                buttons.push(getButton(i));
+            }
+
+            // render last button
+            if (maxPageButtons < pageCount) {
+                buttons.push(<Button key="..." text="..." disabled width={width} height={height} align='center' />);
+                buttons.push(getButton(pageCount));
+            }
+        } else if (pageCount - currentPage < maxPageButtons) {  // don't render button to last page
+            // render first button
+            buttons.push(getButton(1));
+            buttons.push(<Button key="..." text="..." disabled width={width} height={height} align='center' />);
+
+            for (let i = pageCount - maxPageButtons; i <= pageCount; i++) {
+                buttons.push(getButton(i));
+            }
+        } else {  // render buttons to first and last pages
+            // render first button
+            buttons.push(getButton(1));
+            buttons.push(<Button key="first..." text="..." disabled width={width} height={height} align='center' />);
+
+            for (let i = currentPage - Math.ceil(maxPageButtons / 2); i <= currentPage + Math.floor(maxPageButtons / 2); i++) {
+                buttons.push(getButton(i));
+            }
+
+            buttons.push(<Button key="second..." text="..." disabled width={width} height={height} align='center'  />);
+            buttons.push(getButton(pageCount));
+        }
+
+        return buttons;
+    };
+
     return (
         <div className="center-flex gap v-margin">
             <Button
                 onClick={prevPage}
-                disabled={currentPage <= 0}
+                disabled={currentPage <= 1}
                 tooltip="Previous Page"
                 Icon={<NavigateBeforeIcon />}
             />
-            { pageCount > 0 && <Button text={currentPage + 1} /> }
+            { getPageButtons() }
             <Button
                 onClick={nextPage}
-                disabled={currentPage >= pageCount - 1}
+                disabled={currentPage >= pageCount}
                 tooltip="Next Page"
                 Icon={<NavigateNextIcon />}
             />
