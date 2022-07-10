60921a6f815ad7a73a8b55d6e25099b7de590c66
Kenneth Tran <kennethtran77@gmail.com>
2022-07-10 05:45:38 -0400

    Remove unused line

--- client/src/components/pages/Settings/Settings.js
+++ client/src/components/pages/Settings/Settings.js
@@ -1,4 +1,3 @@
-import { style } from '@mui/system';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateSettings, updateUsername, updatePassword } from '../../../actions/user';
