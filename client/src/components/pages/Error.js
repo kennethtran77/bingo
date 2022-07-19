0be91ad2fb151e7c7c434481ca081d17ea630e7c
Kenneth Tran <kennethtran77@gmail.com>
2022-07-18 20:31:25 -0400

    Visual changes

--- client/src/components/pages/Error.js
+++ client/src/components/pages/Error.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Button from '../widgets/Button';
+
+import HomeIcon from '@mui/icons-material/Home';
 
 const Error = () => {
     return (
-        <h1>
-            404 Unknown Page
-        </h1>
+        <div className="center-flex max-view">
+            <div>
+                <h1>404</h1>
+                <h2>The page you are looking for does not exist.</h2>
+                <Button link="/" text="Go Home" background Icon={<HomeIcon />} />
+            </div>
+        </div>
     );
 }
 
